b19d3dbabbafe31a37325eff1e8c8f07cfb55f76
Aniket6207 <aniketmore6207@gmail.com>
2023-04-23 20:19:41 +0530

    Initial commit

--- criminal.sql
+++ criminal.sql
@@ -1,23 +1,62 @@
-CREATE TABLE `criminal`(
- `Case_id` VARCHAR(45) NOT NULL , 
- `Criminal_id` VARCHAR(45) NULL ,
- `Criminal_name` VARCHAR(45) NULL ,
- `Nick_name` VARCHAR(45) NULL ,
- `arrest_date` VARCHAR(45) NULL ,
- `dateOfcrime` VARCHAR(45) NULL ,
- `address` VARCHAR(45) NULL ,
- `age` VARCHAR(45) NULL ,
- `occupation` VARCHAR(45) NULL ,
- `BirthMark` VARCHAR(45) NULL ,
- `crimeType` VARCHAR(45) NULL ,
- `fatherName` VARCHAR(45) NULL ,
- `gender` VARCHAR(45) NULL ,
- `wanted` VARCHAR(45) NULL ,
-  PRIMARY KEY (`Case_id`))
+create database management;
+use management;
+show tables;
+desc prisoner;
+desc crime;
 
-CREATE TABLE `management`.`login` (
+CREATE TABLE `prisoner`(
+ prisoner_id VARCHAR(45)  NOT NULL,
+ Name VARCHAR(45) NOT NULL ,
+ Address text NULL ,
+ age int(16) NULL ,
+ Jail_id int(20) NOT NULL,
+  PRIMARY KEY (`prisoner_id`),
+  FOREIGN KEY (Jail_id) references Jail(Jail_id));
+  
+
+CREATE TABLE login (
   `userid` VARCHAR(10) NOT NULL,
   `password` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`userid`));
 
-SELECT * FROM management.criminal;
+CREATE TABLE Crime(
+   prisoner_id VARCHAR(45),
+   crime_id int(16) NOT NULL,
+   Type VARCHAR(40) NOT NULL,
+   Crime_details longtext NULL,
+   Location text NULL,
+   PRIMARY KEY (crime_id),
+   FOREIGN KEY (prisoner_id) references prisoner(prisoner_id));
+
+CREATE TABLE Jail(
+   Jail_id int(20) NOT NULL,
+   Jail_name VARCHAR(40) NOT NULL,
+   Jail_Address text NULL,
+   Prisoners_count int(20) NULL,
+   PRIMARY KEY (Jail_id));
+   
+CREATE TABLE staff(
+   Name VARCHAR(45) NOT NULL,
+   Age int(10) NOT NULL,
+   Gender varchar(30) NULL,
+   Designation varchar(45) NOT NULL,
+   staff_id int(16) NOT NULL,
+   Jail_id int(16) NOT NULL,
+   FOREIGN KEY (Jail_id) references Jail(Jail_id),
+   PRIMARY KEY(Jail_id,staff_id));
+   
+CREATE TABLE Punsishments(
+   Type varchar(45) NOT NULL,
+   Details longtext NULL,
+   Duration VARCHAR(45) NULL,
+   IPC_Rule text NOT NULL,
+   prisoner_id VARCHAR(45) NOT NULL,
+   FOREIGN KEY (prisoner_id) references prisoner(prisoner_id));
+   
+
+insert into login values("root","Aniket@62");
+SELECT * FROM login;
+select * from prisoner;
+insert into jail values(21,"California Jail","California",23);
+insert into prisoner values(5,"John","LA",45,21);
+insert into prisoner values(6,"Davies","LA",43,21);
--- login.py
+++ login.py
@@ -6,53 +6,55 @@ from tkinter import messagebox
 from subprocess import call
 
 
+user_name=""
+
+
 class Login_Window:
     def __init__(self,root):
         self.root=root
         self.root.title("Login")
         self.root.geometry("1550x800+0+0")
+        self.login_img=PhotoImage(file='login.png')
+        Label(self.root,image=self.login_img,bg="white").place(x=0,y=0,width=700,height=650)
+        frame=Frame(self.root,bg="white")
+        frame.place(x=550,y=0,width=700,height=650)
 
-        #self.bg=ImageTk.PhotoImage(file=r"C:\Users\Sharwari\Desktop\DBMS MINI PROJECT\images\2.jpeg")
-        #lbl_bg=Label(self.root,image=self.bg)
-        #lbl_bg.place(x=0,y=0,relwidth=1,relheight=1)
-
-        frame=Frame(self.root,bg="black",)
-        frame.place(x=610,y=170,width=340,height=450)
-
-        get_str=Label(frame,text="ADMIN LOGIN",font=("times new roman",20,"bold"),fg="white",bg="black")
-        get_str.place(x=80,y=100)
+        get_str=Label(frame,text="Log in",font=("Microsoft YaHei UI Light",23,"bold"),fg="#57a1f8",bg="white")
+        get_str.place(x=230,y=150)
 
         # Labels
-        username=lbl=Label(frame,text="Username",font=("times new roman",15,"bold"),fg="white",bg="black")
-        username.place(x=50,y=155)
+        username=lbl=Label(frame,text="Username",font=("Microsoft YaHei UI Light",15,"bold"),fg="black",bg="white")
+        username.place(x=150,y=230)
 
         self.txtuser=ttk.Entry(frame,font=("times new roman",15,"bold"))
-        self.txtuser.place(x=40,y=180,width=270)
+        self.txtuser.place(x=150,y=270,width=270)
 
-        password=lbl=Label(frame,text="Password",font=("times new roman",15,"bold"),fg="white",bg="black")
-        password.place(x=50,y=220)
+        password=lbl=Label(frame,text="Password",font=("Microsoft YaHei UI Light",15,"bold"),fg="black",bg="white")
+        password.place(x=150,y=300)
 
         self.txtpass=ttk.Entry(frame,font=("times new roman",15,"bold"), show = "*")
-        self.txtpass.place(x=40,y=250,width=270)
+        self.txtpass.place(x=150,y=340,width=270)
 
-        loginbtn=Button(frame,text="Login",command=self.login, font=("times new roman",15,"bold"),bd=3,relief=RIDGE,fg="white",bg="red",activeforeground="white",activebackground="red")
-        loginbtn.place(x=110,y=300,width=120)
+        loginbtn=Button(frame,text="Login",command=self.login, font=("Microsoft YaHei UI Light",15,"bold"),bd=3,relief=RIDGE,fg="white",bg="#57a1f8",activeforeground="white",activebackground="red")
+        loginbtn.place(x=230,y=400,width=120)
 
     def login(self):
         if self.txtuser.get()=="" or self.txtpass.get()=="":
             messagebox.showerror("Error", "All fields are required")
         else:
             try:
-                con=mysql.connector.connect(host='localhost', username='root',password='Sharwari@21', database='management')
+                con=mysql.connector.connect(host='localhost', username='root',password='Aniket@62', database='management')
                 cur=con.cursor() 
-                cur.execute("select * from login where userid=%s and password=%s", (self.txtuser.get(), self.txtpass.get()))
+                cur.execute("select * from staff where login_id=%s and password=%s", (self.txtuser.get(), self.txtpass.get()))
                 row=cur.fetchone()
+                print(row)
                 if row==None:
                     messagebox.showerror("Error", "Invalid User ID or Password")
                 else:
+                    user_name=self.txtuser.get()
                     messagebox.showinfo("successful", "Welcome") 
                     self.root.destroy()
-                    call(['python','crime management system.py'])                  
+                    call(['python','system_staff.py'])                  
                 con.close()  
             except Exception as er:
                 messagebox.showerror('error',f'Due to {str(er)}')   
